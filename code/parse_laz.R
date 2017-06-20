1ed562fe85f6008554d22d615667c4e8fd16e2a4
Naupaka Zimmerman <naupaka@gmail.com>
2017-06-20 17:34:16 -0600

    Builtin lidR functions are way better, deleted a bunch of stuff

--- code/parse_laz.R
+++ code/parse_laz.R
@@ -21,52 +21,8 @@ if (!require("dplyr")) {
   library("dplyr")
 }
 
-lidar_in <- readLAS("data/2013_SJER_1_252000_4103000_colorized.laz")
+lidar_in <- readLAS("data/2013_SJER_1_256000_4112000_colorized.laz")
 plot(lidar_in)
 
-lidar_data <- lidar_in@data
-
-min_x = min(lidar_data$X)
-max_x = max(lidar_data$X)
-min_y = min(lidar_data$Y)
-max_y = max(lidar_data$Y)
-
-lidar_data[lidar_data$X == 252821]
-
-diff(range(lidar_data$X))
-diff(range(lidar_data$Y))
-diff(range(lidar_data$Z))
-
-
-all_data <- matrix(NA, 0, 9)
-for (bin_sizes in seq(5, 100, by = 5)) {
-  my_lidar_binned <- lidar_data %>%
-    mutate(X_bins = cut(X, breaks = seq(min(lidar_data$X), 
-                                        max(lidar_data$X), 
-                                        by = bin_sizes))) %>%
-    mutate(Y_bins = cut(Y, breaks = seq(min(lidar_data$Y), 
-                                        max(lidar_data$Y), 
-                                        by = bin_sizes))) %>%
-    group_by(X_bins, Y_bins) %>%
-    summarize(max_Z = max(Z),
-              min_Z = min(Z),
-              mean_Z = mean(Z, na.rm = TRUE),
-              range_Z = diff(range(Z)),
-              count_Z = n(),
-              sd_Z = sd(Z)) %>%
-    na.omit() %>%
-    mutate(bin_size = bin_sizes)
-  my_lidar_binned <- as.matrix(my_lidar_binned)
-  all_data <- rbind(all_data, my_lidar_binned)
-} 
-all_data <- as.data.frame(all_data)
-
-all_data %>%
-  group_by(bin_size) %>%
-  summarize(avg_mean_Z = mean(as.numeric(as.character(mean_Z)), na.rm = TRUE),
-            avg_min_Z = mean(as.numeric(as.character(min_Z))),
-            avg_max_Z = mean(as.numeric(as.character(max_Z))),
-            avg_range_Z = mean(as.numeric(as.character(range_Z))),
-            avg_sd_Z = mean(as.numeric(as.character(sd_Z))),
-            avg_count_Z = mean(as.numeric(as.character(count_Z, na.rm = TRUE)))) %>%
-  arrange(as.numeric(as.character(bin_size)))
+hmean <- lidar_in %>% grid_metrics(entropy(Z), res = 10)
+plot(hmean)
